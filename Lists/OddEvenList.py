8836601524056d7a41547a10e24ed483715f22c0
TapasDash <tapasdash017@gmail.com>
2018-09-24 06:01:58 +0530

    Add files via upload
    This is a simple programme which takes in input from the user as a series of numbers 
    then, filter out the even and odd ones and
    append them in two different lists naming them oddList and evenList
    
    Input : 1,2,3,4,5,6
    Output : oddList = [1,3,5]
    		 evenList = [2,4,6]

---
+++ Lists/OddEvenList.py
@@ -0,0 +1,20 @@
+'''
+This is a simple programme which takes in input from the user as a series of numbers 
+then, filter out the even and odd ones and
+append them in two different lists naming them oddList and evenList
+
+Input : 1,2,3,4,5,6
+Output : oddList = [1,3,5]
+		 evenList = [2,4,6] 	
+'''
+myList = [eval(x) for x in input("Enter series of numbers = ").split(",")]
+oddList = []
+evenList = []
+for x in myList:
+	if(x % 2 == 0):
+		evenList.append(x)
+	else:
+		oddList.append(x)
+
+print("Odd List=",oddList)
+print("Even List=",evenList)
